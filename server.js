fb4fa9205adefcbf567d50a672fabb1ae4d78f0e
sps001 <spsdangi001@gmail.com>
2025-03-14 11:03:20 +0530

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+    
+    socket.on('offer', (data) => {
+        socket.broadcast.emit('offer', data);
+    });
+    
+    socket.on('answer', (data) => {
+        socket.broadcast.emit('answer', data);
+    });
+    
+    socket.on('candidate', (data) => {
+        socket.broadcast.emit('candidate', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
 
